17597019443d04b2b44236bf737a94383f36da18
Kawsar Hussen <kawsarhussen@gmail.com>
2019-12-22 14:34:52 -0500

    Adding goal functionality implemented

--- my-app/App.js
+++ my-app/App.js
@@ -1,21 +1,43 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text,TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('');
+  const [enteredTask, setEnteredTask] = useState('');
+  const [goal, setGoal] = useState([]);
+  const handleInput= (input) => setEnteredTask(input);
+  const handleGoal = () => setGoal([...goal, enteredTask]);
+
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title='Change Text' />
+    <View  style= { styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder= 'Course Goal' style={styles.inputBox} onChangeText= {handleInput}/>
+        <Button title='ADD'  onPress={handleGoal}/>
+      </View>
+      <View>
+        {goal.map(goal => (
+          <View style={styles.listItem}> 
+            <Text key={goal} > {goal}</Text> 
+          </View> ) )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen:{
+    padding: 50,
   },
+  inputContainer:{
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+  },
+  inputBox:{
+    margin: 1, padding: 2, borderColor: 'black', borderWidth: 1, width:  '80%'
+  },
+  listItem:{
+    padding: 10,
+    backgroundColor: 'grey',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+  
 });
